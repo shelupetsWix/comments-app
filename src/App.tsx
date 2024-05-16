c3b3cf5f7cb4efa68f7eb251889bee5a09c5d87d
Dmitry Shelupets <dmitry.shelupets@wix.com>
2024-05-16 20:23:12 +0300

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import React from "react";
-import "./App.css";
 import { CommentForm, CommentList } from "./components";
 
-function App() {
-  return (
-    <div className="max-w-2xl mx-auto p-4">
-      <CommentForm />
-      <CommentList />
-    </div>
-  );
-}
-
-export default App;
+export const App = () => (
+  <div className="max-w-2xl mx-auto p-4">
+    <CommentForm />
+    <CommentList />
+  </div>
+);
